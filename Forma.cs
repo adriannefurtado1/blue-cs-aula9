b8cc781d13170c7f2fd7b0eb4a1fcea92e2c42e4
Adrianne Furtado <adrianne.furtado@gmail.com>
2022-06-18 21:17:35 -0300

    first commit

---
+++ Forma.cs
@@ -0,0 +1,70 @@
+﻿
+namespace DesafioDoProfessor
+{
+    public abstract class Forma
+    {
+        public abstract float CalculaDimensao();
+
+    }
+
+    public class Quadrado : Forma
+    {
+        float areaQuadrado = 0.0f;
+        float medidaFace = 0.0f;
+
+        public override float CalculaDimensao()
+        {
+            Console.WriteLine("==[       Calculo da Area do QUADRADO       ]==");
+            Console.Write("Informe a medida de 1 dos lados do quadrado: ");
+            medidaFace = float.Parse(Console.ReadLine());
+
+            areaQuadrado = medidaFace * medidaFace;
+            //areaQuadrado   = Math.Pow(medidaFace, 2);
+           
+            return areaQuadrado;
+            
+        }
+    }
+
+    public class Triangulo : Forma
+    {
+        float alturaTriangulo = 0.0f;
+        float baseTriangulo = 0.0f;
+        float areaTriangulo = 0.0f;
+        public override float CalculaDimensao()
+        {
+
+            Console.WriteLine("==[       Calculo da Area do TRIANGULO      ]==");
+
+            Console.Write("Informe a medida da base do triangulo: ");
+            baseTriangulo = float.Parse(Console.ReadLine());
+
+            Console.Write("Informe a medida da altura do triangulo: ");
+            alturaTriangulo = float.Parse(Console.ReadLine());
+
+            areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+
+            return areaTriangulo;
+
+        }
+    }
+
+    public class Circulo : Forma
+    {
+        float raioCirculo = 0.0f;
+        float numeroPi = 3.14159f;
+        float areaCirculo = 0.00f;
+        public override float CalculaDimensao()
+        {
+
+            Console.WriteLine("==[        Calculo da Area do CIRCULO       ]==");
+
+            Console.Write("Informe o raio do circulo: ");
+            raioCirculo = float.Parse(Console.ReadLine());
+
+            areaCirculo = numeroPi * (raioCirculo * raioCirculo) ;
+
+            return areaCirculo;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace DesafioDoProfessor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===============================================");
+            Console.WriteLine("Qual forma deseja calcular? ");
+            Console.WriteLine("1 - Quadrado");
+            Console.WriteLine("2 - Triangulo");
+            Console.WriteLine("3 - Circulo");
+            int alternativa = int.Parse(Console.ReadLine());
+            float area = 0.0f;
+
+            Quadrado dimQuadrado = new Quadrado();
+            Triangulo dimTriangulo = new Triangulo();
+            Circulo dimCirculo = new Circulo();
+
+            switch (alternativa)
+            {
+                case 1:
+                    
+                    area = dimQuadrado.CalculaDimensao();
+                    break;
+                case 2:
+
+                    area = dimTriangulo.CalculaDimensao();
+                    break;
+
+                case 3:
+                  
+                    area = dimCirculo.CalculaDimensao();
+                    break;
+
+                default:
+
+                    break;
+
+            }
+
+            Console.WriteLine($"Area: {area}");
+            Console.WriteLine("===============================================");
+
+
+        }
+    }
+}
